d2b4d071c3105d1c741eb36a4614944d9033a822
yasmine-hj <yasminehajaji95@gmail.com>
2020-01-22 11:48:57 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -12,5 +12,51 @@ document.addEventListener("DOMContentLoaded", ()=>{
       toyForm.style.display = 'none'
     }
   })
-
+  getToys()
+  renderToys()
 })
+
+function getToys(){
+  return fetch("http://localhost:3000/toys")
+  .then(resp => resp.json())
+  .then(toys => {
+    toys.forEach(toy => {addToys(toy))
+    })
+  })
+}
+
+//HTML toy
+function toyCard(toy) {
+  return `
+    <div class="card">
+      <h2>${toy.name}</h2>
+      <img src=${toy.image} class="toy-avatar" />
+      <button class="like-btn">Like <3</button>
+    </div>
+  `
+}
+
+//HTML append
+const displayToy = (toy) => {
+  const toyCollection = document.getElementById("toy-collection")
+  toyCollection.innerHTML += toyCard(toy)
+}
+
+function renderToys(toy) {
+  let h2 = document.createElement('h2')
+  h2.innerText = toy.name
+  let img = document.createElement('img')
+  img.setAttribute('src', toy.image)
+  img.setAttribute('class', 'toy-avatar')
+  let p = document.createElement('p')
+  p.innerText = `${toy.likes} likes`
+  let btn = document.createElement('button')
+  btn.setAttribute('class', 'like-btn')
+  btn.setAttribute('id', toy.id)
+  btn.innerText = "like"
+  btn.addEventListener('click', (e) => {
+    console.log(e.target.dataset);
+    likes(e)
+  })
+
+
