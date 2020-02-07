eff4bf84247bca089e1d35f530b25a5d24432ea8
your_name <raul.a.figueroa@hotmail.com>
2020-02-07 09:59:36 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 let addToy = false
 
 document.addEventListener("DOMContentLoaded", ()=>{
+  
   const addBtn = document.querySelector('#new-toy-btn')
   const toyForm = document.querySelector('.container')
   addBtn.addEventListener('click', () => {
@@ -12,5 +13,70 @@ document.addEventListener("DOMContentLoaded", ()=>{
       toyForm.style.display = 'none'
     }
   })
-
+  displayToys();
+  addToys();
+  toyLikes();
 })
+
+
+function displayToys(){
+  fetch("http://localhost:3000/toys")
+  .then(response => {
+    return response.json(); 
+  })
+  .then((myJson) => {
+      myJson.forEach(toy => { renderToy(toy);
+        
+      })
+  })
+}
+
+function renderToy(toy){
+  let toyCards = document.getElementById("toy-collection");
+  toyCards.innerHTML += `<div class='card' id="toy-${toy.id}">
+  <h2> Name: ${toy.name}</h2>
+  <img class="toy-avatar" src="${toy.image}" >
+  <p> Likes: ${toy.likes}</p>
+  <button class="like-btn" type="button"> Like! </button>
+  </div>`
+}
+
+function addToys(){
+  let form = document.querySelector(".add-toy-form");
+  const toy = {
+    name: form.name.value,
+    image: form.image.value ,
+    likes: 0
+  }
+
+  let add = document.querySelector(".submit");
+  add.addEventListener("click", function(e){
+    e.preventDefault();
+    fetch("http://localhost:3000/toys", {
+      method: 'POST',
+      body: JSON.stringify(toy),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    })
+    .then(response => {
+      return response.json(); 
+    })
+    .then((myJson) => {
+        myJson.forEach(toy => { renderToy(toy);
+          
+        })
+    })
+  })
+}
+
+function toyLikes(){
+  let toyCards = document.querySelectorAll(".card");
+     toyCards.forEach( toy => {
+       let likes = toy.querySelector(".like-btn")
+       likes.addEventListener("click", function(e){
+         console.log("e", e)
+     })
+  })
+}
